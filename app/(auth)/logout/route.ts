1de26e82275369396f1243416393d5d8199e4554
kai <kai4avaya@gmail.com>
2025-11-20 17:10:30 -0500

    up

--- app/(auth)/logout/route.ts
+++ app/(auth)/logout/route.ts
@@ -3,7 +3,7 @@ import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 import { NextRequest } from 'next/server'
 
-export async function POST(request: NextRequest) {
+export async function GET() {
   const supabase = await createClient()
 
   // Sign out the user
